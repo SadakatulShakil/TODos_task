b074ee94338e3c91b25edce6d72c43be5a279fdb
SadakatulShakil <sadakatulshakil94@gmail.com>
2024-01-16 12:50:08 +0600

    facing chlanges setup firebase with CLI, but finally setuped perfectly and run the project, insert, get, update, delete from online. Note: Commit from remote

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,13 @@ import 'package:to_dos/welcome_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+        apiKey: 'AIzaSyCMzComVWU5plAj2FxxMBB5Wo1k2pix_AU',
+        appId: '1:78221884421:android:d9b4b0d0764a8bf10df06c',
+        messagingSenderId: '78221884421',
+        projectId: 'todos-fa13a')
+  );
   runApp(MyApp());
 }
 
